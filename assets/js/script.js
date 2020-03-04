60620e61ed9a936b0d7bfb0f48f2c18d4c47ed0d
Igor <igor.apfentaler@gmail.com>
2020-03-04 17:50:30 +0100

    Fix for input
    last letter is workong now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,71 @@
-var api ="https://itunes.apple.com/search?term=";
+var api = "https://itunes.apple.com/search?term=";
 
-document.getElementById('input').addEventListener('keypress', getApi);
+document.getElementById("input").addEventListener("keyup", getApi);
 
 var song;
-var statusMsg = document.getElementById('status');
+var statusMsg = document.getElementById("status");
 
+function getApi() {
+  var input = document.getElementById("input");
+  var table = document.getElementById("table");
 
-function getApi(){
-    var input = document.getElementById('input');
-    var table = document.getElementById('table');
+  song = input.value;
+  input.innerHTML = "";
 
-    song = input.value;
-    input.innerHTML = '';
+  var url = api + song;
 
-    var url = api + song;
-
-    var poziv = new XMLHttpRequest();
-    poziv.open('GET', url, true);
-    poziv.onload = function (){
-        var data = JSON.parse(poziv.response);
-        statusMsg.innerText = '';
-        getList(data);
-    }
-    table.innerHTML = '';
-    statusMsg.innerHTML = '<img src="assets/img/spiner.svg"> <p>Loading...<p>';
-    poziv.send();
+  var poziv = new XMLHttpRequest();
+  poziv.open("GET", url, true);
+  poziv.onload = function() {
+    var data = JSON.parse(poziv.response);
+    statusMsg.innerText = "";
+    getList(data);
+  };
+  table.innerHTML = "";
+  statusMsg.innerHTML = '<img src="assets/img/spiner.svg"> <p>Loading...<p>';
+  poziv.send();
 }
 
-function getList(data){
-    if(data.resultCount > 0){
-        var table = document.getElementById('table');
-        table.innerHTML = '';
-        for(i = 0; i < data.resultCount; i++){ 
-            var artworkLink = data.results[i].artworkUrl60;
-            var songPlay = data.results[i].previewUrl;
+function getList(data) {
+  if (data.resultCount > 0) {
+    var table = document.getElementById("table");
+    table.innerHTML = "";
+    for (var i = 0; i < data.resultCount; i++) {
+      var artworkLink = data.results[i].artworkUrl60;
+      var songPlay = data.results[i].previewUrl;
 
-            var tr = document.createElement('tr');
-            var albumImg = document.createElement('img');
-            albumImg.setAttribute("src", artworkLink);
+      var tr = document.createElement("tr");
+      var albumImg = document.createElement("img");
+      albumImg.setAttribute("src", artworkLink);
 
-            var playSample = document.createElement('audio');
-            playSample.setAttribute("controls", "");
-            var audioSrc = document.createElement('source');
-            audioSrc.setAttribute("type"," audio/ogg");
-            audioSrc.setAttribute("src", songPlay);
-            playSample.appendChild(audioSrc);
+      var playSample = document.createElement("audio");
+      playSample.setAttribute("controls", "");
+      var audioSrc = document.createElement("source");
+      audioSrc.setAttribute("type", " audio/ogg");
+      audioSrc.setAttribute("src", songPlay);
+      playSample.appendChild(audioSrc);
 
-            var tdArtwork = document.createElement('td');
-            var tdSongInfo = document.createElement('td');
-            var artist = document.createElement('p');
-            var song = document.createElement('p');
-            var album = document.createElement('p');
-            tdSongInfo.append(artist, song, album);
-            var tdSongPlay = document.createElement('td');
-            tdSongPlay.appendChild(playSample);
+      var tdArtwork = document.createElement("td");
+      var tdSongInfo = document.createElement("td");
+      var artist = document.createElement("p");
+      var song = document.createElement("p");
+      var album = document.createElement("p");
+      tdSongInfo.append(artist, song, album);
+      var tdSongPlay = document.createElement("td");
+      tdSongPlay.appendChild(playSample);
 
-            tdArtwork.appendChild(albumImg);
-            tr.appendChild(tdArtwork);
-            tr.appendChild(tdSongInfo);
-            tr.appendChild(tdSongPlay);
+      tdArtwork.appendChild(albumImg);
+      tr.appendChild(tdArtwork);
+      tr.appendChild(tdSongInfo);
+      tr.appendChild(tdSongPlay);
 
-            artist.innerText = data.results[i].artistName;
-            song.innerText = data.results[i].trackName;
-            album.innerText = data.results[i].collectionName;
-            table.appendChild(tr);
-        }
-    } else{
-        statusMsg.innerText = '';
-        statusMsg.innerText = 'Nema rezultata';
+      artist.innerText = data.results[i].artistName;
+      song.innerText = data.results[i].trackName;
+      album.innerText = data.results[i].collectionName;
+      table.appendChild(tr);
     }
+  } else {
+    statusMsg.innerText = "";
+    statusMsg.innerText = "Nema rezultata";
   }
+}
